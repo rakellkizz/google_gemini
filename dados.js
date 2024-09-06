8af9641efa6c5037d5aeb0f61d30de0bc366dd3a
RakellSouza <64821563+rakellkizz@users.noreply.github.com>
2024-09-06 18:15:55 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,25 @@
+//Array de objetos que representam dados de atletas
+let dados = [
+  // Objeto representando Rayssa Leal
+  {
+    titulo: "Rayssa Leal", // Título da atleta
+    descricao: "Rayssa Leal, conhecida como Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela se tornou um ícone do esporte e uma inspiração para jovens atletas.", // Descrição da atleta
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal", // Link para a página da Wikipedia da atleta
+    tags: "skate fada sk8"
+  },
+  // Objeto representando Beatriz Souza
+  {
+    titulo: "Beatriz Souza", // Título da atleta
+    descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua força e técnica. Com diversas conquistas em sua carreira, ela é um dos grandes nomes do judô brasileiro.", // Descrição da atleta
+    link: "https://pt.wikipedia.org/wiki.org/wiki/Beatriz_Souza", // Link para a página da Wikipedia da atleta
+    tags: "judô judo judoca judoka"
+
+  },
+  // Objeto representando Rebeca Andrade
+  {
+    titulo: "Rebeca Andrade", // Título da atleta
+    descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o coração dos brasileiros com sua habilidade, carisma e determinação. Com uma trajetória marcada por superação e conquistas, ela se tornou um dos maiores nomes da ginástica artística do país.", // Descrição da atleta
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade", // Link para a página da Wikipedia da atleta
+    tags: "ginástica ginastica ouro"
+  }
+];
